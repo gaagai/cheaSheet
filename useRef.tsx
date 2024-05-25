68194906666149df7b8786b5f4bd4ed0e3cc0036
gaagai <gal@nbv.co.il>
2024-05-25 19:00:28 +0300

    Update useRef.tsx

--- useRef.tsx
+++ useRef.tsx
@@ -11,3 +11,29 @@ const InputFocus = () => {
 };
 
 export default InputFocus;
+ //***//
+// Mutable value:
+import React, { useRef } from 'react';
+
+const Timer = () => {
+    const timerRef = useRef(0);
+
+    const startTimer = () => {
+        timerRef.current = setInterval(() => {
+            console.log('Timer running...');
+        }, 1000);
+    };
+
+    const stopTimer = () => {
+        clearInterval(timerRef.current);
+    };
+
+    return (
+        <div>
+            <button onClick={startTimer}>Start Timer</button>
+            <button onClick={stopTimer}>Stop Timer</button>
+        </div>
+    );
+};
+
+export default Timer;
