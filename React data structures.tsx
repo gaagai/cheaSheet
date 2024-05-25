503648f62ff65d71fe04c33d7d634e3e06ed14d5
gaagai <gal@nbv.co.il>
2024-05-25 18:31:39 +0300

    Update React data structures.tsx

--- React data structures.tsx
+++ React data structures.tsx
@@ -1,1 +1,16 @@
+// Example: Rendering a list of items
+const ItemList = ({ items }) => {
+    return (
+        <ul>
+            {items.map(item => (
+                <li key={item.id}>{item.name}</li>
+            ))}
+        </ul>
+    );
+};
 
+// Example: Filtering and reducing data
+const totalActiveUsersAge = users
+    .filter(user => user.isActive)
+    .reduce((total, user) => total + user.age, 0);
+//  Methods: map, filter, reduce, forEach, find, some, every, concat, slice, splice.
