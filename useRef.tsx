0428cb5402bd853bc6cee3387f2bf20f2ce11b20
gaagai <gal@nbv.co.il>
2024-05-27 04:06:59 -0700

    Update useRef.tsx

--- useRef.tsx
+++ useRef.tsx
@@ -1,39 +1,42 @@
 import React, { useRef, useEffect } from 'react';
 
-const InputFocus = () => {
-    const inputRef = useRef(null);
+const InputFocus: React.FC = () => {
+  const inputRef = useRef<HTMLInputElement>(null);
 
-    useEffect(() => {
-        inputRef.current.focus();
-    }, []);
+  useEffect(() => {
+    inputRef.current?.focus();
+  }, []);
 
-    return <input ref={inputRef} type="text" />;
+  return <input ref={inputRef} type="text" />;
 };
 
 export default InputFocus;
+
  //***//
 // Mutable value:
 import React, { useRef } from 'react';
 
-const Timer = () => {
-    const timerRef = useRef(0);
+const Timer: React.FC = () => {
+  const timerRef = useRef<NodeJS.Timeout | null>(null);
 
-    const startTimer = () => {
-        timerRef.current = setInterval(() => {
-            console.log('Timer running...');
-        }, 1000);
-    };
+  const startTimer = () => {
+    timerRef.current = setInterval(() => {
+      console.log('Timer running...');
+    }, 1000);
+  };
 
-    const stopTimer = () => {
-        clearInterval(timerRef.current);
-    };
+  const stopTimer = () => {
+    if (timerRef.current) {
+      clearInterval(timerRef.current);
+    }
+  };
 
-    return (
-        <div>
-            <button onClick={startTimer}>Start Timer</button>
-            <button onClick={stopTimer}>Stop Timer</button>
-        </div>
-    );
+  return (
+    <div>
+      <button onClick={startTimer}>Start Timer</button>
+      <button onClick={stopTimer}>Stop Timer</button>
+    </div>
+  );
 };
 
 export default Timer;
